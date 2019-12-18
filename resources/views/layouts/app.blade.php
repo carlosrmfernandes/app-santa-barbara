d8310e89ad7bd80c8808c7fa27f6e182f5d6915a
carlosfernandes <carlosfernandes@192.168.1.182>
2019-12-17 22:22:04 -0300

    Project Update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,7 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
+        <!--value="http://localhost:8888"-->
         <input type="text" style="display: none" id="endereco_servidor_api" value="http://localhost:8888"/>
         <div id="app">
             <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
